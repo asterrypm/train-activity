40992534cd1c5190f097f66d261cb5b879fef9a4
asterrypm <asterrypm@gmail.com>
2018-03-05 17:09:53 -0500

    basic html, css,

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # train-activity
 train activity using momentjs
+
---
+++ assets/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDXpLXchZHHxLLRcaKf2rVAPnBBuWv4qNk",
+    authDomain: "train-project-ea895.firebaseapp.com",
+    databaseURL: "https://train-project-ea895.firebaseio.com",
+    projectId: "train-project-ea895",
+    storageBucket: "",
+    messagingSenderId: "344921288630"
+  };
+  firebase.initializeApp(config);
